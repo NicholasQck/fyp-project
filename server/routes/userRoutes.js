e4fdd1a8110e1cfb55d4693dc49223177e192e1c
NicholasQck <nicholasng7138@gmail.com>
2023-09-27 17:12:43 +0800

    removed unused code

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -13,8 +13,6 @@ import {
 
 export const usersRouter = express.Router();
 
-// usersRouter.use('/', validateUser);
-
 usersRouter.route('/').get(getAllUsers).post(validateUser).post(createUser);
 usersRouter.route('/roles').get(getAllRoles);
 usersRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
