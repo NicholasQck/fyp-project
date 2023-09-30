f67f9a41db9954a174af31cb62437a1ef16e7567
NicholasQck <nicholasng7138@gmail.com>
2023-09-30 21:03:12 +0800

    reorganized routes as private routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,18 @@ import Titles from './pages/main/Titles';
 import CreateUser from './pages/dashboard/CreateUser';
 import Error from './pages/error/Error';
 
+// import components
+import PrivateRoutes from './components/PrivateRoutes';
+
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/titles" element={<Titles />} />
-        <Route path="/create_user" element={<CreateUser />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/titles" element={<Titles />} />
+          <Route path="/users/create_user" element={<CreateUser />} />
+        </Route>
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
