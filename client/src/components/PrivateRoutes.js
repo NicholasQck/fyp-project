d75bf64bcac754f7e6002fa2f6339410a40896e0
NicholasQck <nicholasng7138@gmail.com>
2023-09-30 20:59:35 +0800

    added private route component

---
+++ client/src/components/PrivateRoutes.js
@@ -0,0 +1,9 @@
+import { Outlet, Navigate } from 'react-router-dom';
+
+const PrivateRoutes = () => {
+  const user = localStorage.getItem('user');
+
+  return user ? <Outlet /> : <Navigate to="/" />;
+};
+
+export default PrivateRoutes;
