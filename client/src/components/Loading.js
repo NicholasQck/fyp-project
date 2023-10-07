2580894a2d93173adf322e8e7b9d0090936e520e
NicholasQck <nicholasng7138@gmail.com>
2023-10-08 00:08:44 +0800

    added loading UI

---
+++ client/src/components/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import assets
+import '../assets/loading.css';
+
+const Loading = () => {
+  return (
+    <div className="spinner-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+};
+
+export default Loading;
