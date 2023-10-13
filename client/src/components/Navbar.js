b1071ec137c2c6bda0b910affa33539e5c2ac693
NicholasQck <nicholasng7138@gmail.com>
2023-10-13 13:45:14 +0800

    created the navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+// import assets
+import '../assets/navbar.css';
+import logo from '../assets/images/logo.png';
+import announceLogo from '../assets/images/announceLogo.png';
+import chatLogo from '../assets/images/chatLogo.png';
 
 const Navbar = () => {
   return (
     <nav>
-      <h1>Navbar</h1>
+      <div className="nav-center">
+        <div className="main-logo">
+          {/* add a link component */}
+          <Link to={'/titles'} className="disable-default-link">
+            <img src={logo} alt="Logo" />
+          </Link>
+        </div>
+
+        <ul className="page-links">
+          <Link
+            to={'/announcements'}
+            className="disable-default-link link-flex"
+          >
+            <li className="nav-list-item">
+              <img src={announceLogo} alt="Announcement Logo" />
+              <p>Announcements</p>
+            </li>
+          </Link>
+
+          <Link to={'/chat'} className="disable-default-link link-flex">
+            <li className="nav-list-item">
+              <img src={chatLogo} alt="Chat Logo" />
+              <p>Chat</p>
+            </li>
+          </Link>
+
+          <Link
+            to={'/users/create_user'}
+            className="disable-default-link link-flex"
+          >
+            <li className="nav-list-item">
+              <div className="round-profile">
+                <p>S</p>
+              </div>
+            </li>
+          </Link>
+        </ul>
+      </div>
     </nav>
   );
 };
